fd69881e17d515e2d308c9fc032181d5dc31ba0c
xiaohangxiaobaibai <40209384+xiaohangxiaobaibai@users.noreply.github.com>
2018-12-08 03:04:16 +0000

    demo9

--- pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
-022\# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
-    'name': 'PS Cloud 培训',
+    'name': 'PS Cloud 培训byliuxh',
     'version': '12.0.1.0',
     'summary': 'PS Cloud 培训',
     'author': "www.mypscloud.com",
